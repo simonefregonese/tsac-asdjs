f1f20ebaf5e78b3566fe53c1d3cf880e4410e38b
simonefregonese <simonefregonese2@hotmail.it>
2015-11-19 20:32:28 +0100

    19/11/2015

--- homework_01.js
+++ homework_01.js
@@ -75,6 +75,19 @@ function ex_2_R(x) {
     return firstOddR(x);
 }
 
+function createArray(n){
+	var myarray=[];
+	for(var i=0;i<myarray.length;i++){
+		myarray.push(i);
+	}
+	return myarray;
+}
+function ex_2_F(n){
+	createArray(2 * n)
+	.filter(x => x % 2 != 0)
+	.reduce((acc,x) => acc+x,0);
+
+}
 
 
 function ex_3_I(a){
@@ -84,22 +97,77 @@ function ex_3_I(a){
     return somma/i;                  
 }
 
-
-function ex_3_R(a){
-    if(a[0]==0)
+function sommaR(myarray){
+	if(myarray.length==0)
 	return 0;
-    else
-	return (a[0]+a.slice(1))/a.length;
+	else
+	return myarray[0] + sommaR(myarray.slice(1));
 }
 
-function ex_4_I(n1,n2){
-	somma=0;    
-	for(i=0;i<n2-n1;i++){
-		somma+=n1; 	
-		n1++;
-	}
-	return somma;	
+function ex_3_R(myarray){
+
+	return sommaR(myarray)/myarray.length;
 }
+
+function ex_3_F(myarray){
+	var tot=0;
+	myarray.forEach(x => {tot += x;});  // =>{} = function(x){}
+	return tot / myarray.length;
+
+	/*con reduce:
+		return myarray.reduce((acc,x) => {return acc + x;},0) / myarray.length;
+			*/
+}
+function ex_4_I(n1,n2){
+	function ex_4_Inner(n1,n2){
+		somma=0;    
+		for(i=n1;i<=n2;i++){
+			somma+=i; 	
+		}
+		return somma;	
+	}
+	
+	if(a>b)
+	return ex_4_Inner(a,b);
+	else
+	return ex_4_Inner(b,a);
+}
+
+
+
+
+function ex_4_R(a,b){
+	function ex_4_Inner(a,b){
+		if(a == b){
+			return b;
+		}
+		else
+		return a + ex_4_Inner(a+1,b); 
+	}
+	
+	if(a>b)
+	return ex_4_Inner(a,b);
+	else
+	return ex_4_Inner(b,a);
+}
+
+function createArrayFrom(a,b){
+	var myarray=[];
+	for(var i=a;i<b;i++){
+		myarray.push(i);
+	}
+	return myarray;
+}
+
+function ex_4_F(a,b){
+	if(a<b){
+		var t=a;
+		a=b;
+		b=t;
+	}
+	return createArrayFrom(a,b).reduce((acc,x) => acc+x,0);
+}
+
 
 
 function ex_5_I(n1,n2){
@@ -120,10 +188,21 @@ function ex_5_R(n1,n2){
 	return n1 + ex_5_R(n1,n2-1);
 }
 
+function createArrayMul(a,b){
+	var myarray=[];
+	for(var i=0;i<b;i++){
+		myarray.push(a);
+	}
+	return myarray;
+}
 
-function ex_6_I(div1,div2){
-	resto=0;
-	quoz=0;
+function ex_5_F(a,b){
+	return createArrayMul(a,b).reduce((acc,x) => acc + x,0);
+	
+}
+
+function ex_6_I(a,b){
+	/*
 	if(div1>0 && div2>0){
 		for(i=0;i<=div1;i++){
 			quoz=quoz+i;
@@ -131,16 +210,34 @@ function ex_6_I(div1,div2){
 		}
 		resto=quoz-div1;
 	}
-	return console.log(quoz + ' con resto ' + resto);
+	return console.log(quoz + ' con resto ' + resto);*/
+	var resto=0;
+	var quoz=0;
+	while(a>=b){
+		a=a-b;
+		quoz++;
+	}
+	resto=a;
+	return [quoz, resto];
 }
  
 
+function ex_6_R(a,b){
+	if(a<=b)
+	return [0,a];
+	else 
+	var result=ex_6_R(a-b,b); //a/b = ((a-b)/b ) + 1
+	return [result[0] + 1, result[1]];
+}
 
+/*function ex_6_F(a,b){
+	
+}*/
 
 function ex_7_I(n,esp){
-    potenza=0;
+ 
     for(i=0;i<esp;i++)
-        potenza= potenza + ex_5(n,n-1);
+       potenza= ex_5_I(n,esp);
     return potenza;                
                     
 }
@@ -150,18 +247,29 @@ function ex_7_R(n,esp){
 	if(n<=0)
 		return 0;
 	else
-		return n * ex_7_R(n,esp-1);
+		return ex_5_R(n,ex_7_R(n,esp-1));
+}
+
+function ex_7_F(n,esp){
+	return createArrayMul(a,b).reduce((acc,x) => acc+x,0);
 }
 
 
 function ex_8_I(a){
+	var m=[];
+	var n=Math.sqrt(a.length);
 	for(i=0;i<a.length;i++){
 		for(j=0;j<a.length;j++){
-
+			m[i]=a.slice(i*n,(i+1)*2);
 		}
 
 	}
 
+}
+
+
+function ex_8_R(a){
+	//trasformare in matrice un vettore più piccolo; da n elementi in n colonne da n.
 }
 
 
@@ -208,6 +316,7 @@ function ex_11_I(a){
 	}
 	return b;
 }
+
 
 
 
--- homework_02.js
+++ homework_02.js
@@ -23,32 +23,68 @@ function returnWithstop(a,f,x){
 }
 
 
-function ex_2_F(myarray){
-
+function createArray(n){
+	var myarray=[];
+	for(var i=0;i<myarray.length;i++){
+		myarray.push(i);
+	}
+	return myarray;
 }
+function ex_2_F(n){
+	createArray(2 * n)
+	.filter(x => x % 2 != 0)
+	.reduce((acc,x) => acc+x,0);
 
-function ex_2_F(myarray){
-    
 }
 
 function ex_3_F(myarray){
+	var tot=0;
+	myarray.forEach(x => {tot += x;});  // =>{} = function(x){}
+	return tot / myarray.length;
+
+	/*con reduce:
+		return myarray.reduce((acc,x) => {return acc + x;},0) / myarray.length;
+			*/
+}
+
+
+function createArrayFrom(a,b){
+	var myarray=[];
+	for(var i=a;i<b;i++){
+		myarray.push(i);
+	}
+	return myarray;
+}
+
+function ex_4_F(a,b){
+	if(a<b){
+		var t=a;
+		a=b;
+		b=t;
+	}
+	return createArrayFrom(a,b).reduce((acc,x) => acc+x,0);
+}
+
+
+function createArrayMul(a,b){
+	var myarray=[];
+	for(var i=0;i<b;i++){
+		myarray.push(a);
+	}
+	return myarray;
+}
+
+function ex_5_F(a,b){
+	return createArrayMul(a,b).reduce((acc,x) => acc + x,0);
+	
+}
+
+function ex_6_F(a,b){
     
 }
 
-function ex_4_F(myarray){
-    
-}
-
-function ex_5_F(myarray){
-    
-}
-
-function ex_6_F(myarray){
-    
-}
-
-function ex_7_F(myarray){
-    
+function ex_7_F(n,esp){
+	return createArrayMul(a,b).reduce((acc,x) => acc+x,0);
 }
 
 function ex_8_F(myarray){
